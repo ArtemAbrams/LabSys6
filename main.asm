8b7b8110a456a29b77eb6859c15dd363f1a9d1be
artem <barishartem@gmail.com>
2023-11-17 20:49:28 +0200

    change asembler code

--- main.asm
+++ main.asm
@@ -1,71 +1,27 @@
 sumOfDivisors(int):
-        push    rbp
-        mov     rbp, rsp
-        mov     DWORD PTR [rbp-20], edi
-        mov     DWORD PTR [rbp-4], 0
-        mov     DWORD PTR [rbp-8], 1
+        pushq   %rbp
+        movq    %rsp, %rbp
+        subq    $16, %rsp
+        movl    %ecx, 16(%rbp)
+        movl    $0, -4(%rbp)
+        movl    $1, -8(%rbp)
         jmp     .L2
 .L4:
-        mov     eax, DWORD PTR [rbp-20]
-        cdq
-        idiv    DWORD PTR [rbp-8]
-        mov     eax, edx
-        test    eax, eax
+        movl    16(%rbp), %eax
+        cltd
+        idivl   -8(%rbp)
+        movl    %edx, %eax
+        testl   %eax, %eax
         jne     .L3
-        mov     eax, DWORD PTR [rbp-8]
-        add     DWORD PTR [rbp-4], eax
+        movl    -8(%rbp), %eax
+        addl    %eax, -4(%rbp)
 .L3:
-        add     DWORD PTR [rbp-8], 1
+        addl    $1, -8(%rbp)
 .L2:
-        mov     eax, DWORD PTR [rbp-8]
-        cmp     eax, DWORD PTR [rbp-20]
+        movl    -8(%rbp), %eax
+        cmpl    16(%rbp), %eax
         jle     .L4
-        mov     eax, DWORD PTR [rbp-4]
-        pop     rbp
-        ret
-.LC0:
-        .string "Sum of division "
-main:
-        push    rbp
-        mov     rbp, rsp
-        push    r12
-        push    rbx
-        sub     rsp, 16
-        mov     DWORD PTR [rbp-20], 20
-        jmp     .L7
-.L10:
-        mov     DWORD PTR [rbp-24], 500
-        jmp     .L8
-.L9:
-        mov     esi, OFFSET FLAT:.LC0
-        mov     edi, OFFSET FLAT:_ZSt4cout
-        call    std::basic_ostream<char, std::char_traits<char> >& std::operator<< <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*)
-        mov     r12, rax
-        mov     eax, DWORD PTR [rbp-20]
-        mov     edi, eax
-        call    sumOfDivisors(int)
-        mov     ebx, eax
-        mov     eax, DWORD PTR [rbp-24]
-        mov     edi, eax
-        call    sumOfDivisors(int)
-        add     eax, ebx
-        mov     esi, eax
-        mov     rdi, r12
-        call    std::basic_ostream<char, std::char_traits<char> >::operator<<(int)
-        mov     esi, OFFSET FLAT:_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
-        mov     rdi, rax
-        call    std::basic_ostream<char, std::char_traits<char> >::operator<<(std::basic_ostream<char, std::char_traits<char> >& (*)(std::basic_ostream<char, std::char_traits<char> >&))
-        add     DWORD PTR [rbp-24], 1
-.L8:
-        cmp     DWORD PTR [rbp-24], 999
-        jle     .L9
-        add     DWORD PTR [rbp-20], 1
-.L7:
-        cmp     DWORD PTR [rbp-20], 999
-        jle     .L10
-        mov     eax, 0
-        add     rsp, 16
-        pop     rbx
-        pop     r12
-        pop     rbp
+        movl    -4(%rbp), %eax
+        addq    $16, %rsp
+        popq    %rbp
         ret
